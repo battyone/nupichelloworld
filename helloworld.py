5a176182695da0b28f83928ffc1e2d61053aac83
Kevin Martin Jose <youcancallmekevin@gmail.com>
2014-02-22 21:54:40 +0530

    Now generates random array of binary elements as the input vector

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,19 @@
 from nupic.research.flat_spatial_pooler import FlatSpatialPooler as FP
 from nupic.research.spatial_pooler import SpatialPooler
 import numpy as np
+from random import randrange
 
 print "hello"
 
 flat=FP()
-n=np.array([1]*1024)
-
-print np.size(n)
-flat.compute(n,True,n)
+n=np.array([0])
+act=np.array([0])
+for i in range(1023):	#An array of 0s and 1s generated randomly. 1024 long
+  n=np.append(n,[randrange(2)])
+for i in range(64*64):
+  active=np.append(active,[0])	#64*64 because that is the number of columns. Created just to supply it as a parameter to compute()
+ 
+print np.size(n) 
+print np.size(act)
+flat.compute(n,True,act)
+flat.printFlatParameters()
