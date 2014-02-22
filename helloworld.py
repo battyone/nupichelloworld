0aec7aff2ae09365110f7145aad24851f193a163
Kevin Martin Jose <youcancallmekevin@gmail.com>
2014-02-22 20:26:57 +0530

    corrected initializing error

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,10 @@
 from nupic.research.flat_spatial_pooler import FlatSpatialPooler as FP
+from nupic.research.spatial_pooler import SpatialPooler
 import numpy as np
 
 print "hello"
 
-flat=FP((2,2))
+flat=FP()
 n=np.array([1]*1024)
 
 print np.size(n)
